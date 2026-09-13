b4de1bcbe00fbf6c4d7d990207a40e52b214d988
Enayatullah Khalili <enayatkhalili98@yahoo.com>
2026-09-14 00:41:34 +0430

    reverse engin
    go for it

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -42,6 +42,26 @@ router.get(
 );
 // FACEBOOK AUTH
 router.get(
+  "/facebook/callback",
+  (req, res, next) => {
+    console.log("========== FACEBOOK CALLBACK HIT ==========");
+    console.log("TIME:", new Date().toISOString());
+    console.log("CODE EXISTS:", !!req.query.code);
+    console.log("URL:", req.originalUrl);
+    console.log("============================================");
+
+    next();
+  },
+  passport.authenticate("facebook", {
+    failureRedirect: "/login",
+  }),
+  (req, res) => {
+    console.log("========== FACEBOOK LOGIN SUCCESS ==========");
+    res.redirect("/");
+  },
+);
+
+router.get(
   "/facebook",
   passport.authenticate("facebook", {
     scope: ["email"],
